5ff5f4be936d601ca917893a26536df123182c36
alsusaifetdinova <Alsu102@yahoo.com>
2018-11-11 01:14:12 -0500

    created foodServicesView from employees side

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("FloorChart.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FoodServicesView.fxml"));
         primaryStage.setTitle("FGCU Inn");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 1280));
         primaryStage.show();
     }
 
